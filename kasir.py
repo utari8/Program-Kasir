d097aca1ac3685957cc0ca88cb2789fe12aa307c
F. N. Utari <77846876+utari8@users.noreply.github.com>
2021-12-12 15:36:19 +0700

    Update kasir.py

--- kasir.py
+++ kasir.py
@@ -15,35 +15,43 @@ print("SUSU        : Rp. 18000")
 
 jml = []
 fd = []
+k_jml = []
 
 def kasir():
     if pesan == "soto" or pesan == "SOTO":
         print(makan[0]," : ", h_mkn[0])
         jml.append(15000)
+        k_jml.append(kali)
         fd.append("SOTO")
     elif pesan == "sate" or pesan == "SATE":
         print(makan[1]," : ", h_mkn[1])
         jml.append(20000)
+        k_jml.append(kali)
         fd.append("SATE")
     elif pesan == "nasi goreng" or pesan == "NASI GORENG":
         print(makan[2]," : ", h_mkn[2])
         jml.append(25000)
+        k_jml.append(kali)
         fd.append("NASI GORENG")
     elif pesan == "mi goreng" or pesan == "MI GORENG":
         print(makan[3]," : ", h_mkn[3])
         jml.append(35000)
+        k_jml.append(kali)
         fd.append("MI GORENG")
     elif pesan == "teh" or pesan == "TEH":
         print(minum[0]," : ", h_mn[0])
         jml.append(10000)
+        k_jml.append(kali)
         fd.append("TEH")
     elif pesan == "kopi" or pesan == "KOPI":
         print(minum[1]," : ", h_mn[1])
         jml.append(15000)
+        k_jml.append(kali)
         fd.append("KOPI")
     elif pesan == "susu" or pesan == "SUSU":
         print(minum[2]," : ", h_mn[2])
         jml.append(18000)
+        k_jml.append(kali)
         fd.append("SUSU")
     else:
         print("Tolong masukkan pesanan dengan benar")
@@ -57,18 +65,21 @@ h_mkn = [15000, 20000, 25000, 35000, 30000]
 
 if pilih == "y":
     pesan = str(input("Anda ingin pesan apa? : "))
+    kali = int(input("Jumlahnya berapa? (angka) : "))
     kasir()
     ask = input("Apakah anda ingin memesan kembali (y/n) : ")
     while ask == "y":
         pesan = str(input("Anda ingin pesan apa? : "))
+        kali = int(input("Jumlahnya berapa? (angka) : "))
         kasir()
         ask = input("Apakah anda ingin memesan kembali (y/n) : ")
     if ask == "n":
         print("Pesanan anda : ")
         for i in range(len(fd) and len(jml)):
-            print(fd[i]," : ",jml[i])
+            print(fd[i]," : ",jml[i]," sebanyak ",k_jml[i])
         print("----------------------")
-        print("Total pesanan anda : ", sum(jml))
+        tot = [a*b for a,b in zip(jml,k_jml)]
+        print("Total pesanan anda : ", sum(tot))
         print("Terimakasih sudah datang")
 else:
     print("Terimakasih sudah datang")
